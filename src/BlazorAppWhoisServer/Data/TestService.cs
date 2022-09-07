50bdfcffa4aa829b32d434a788d2dc6b55518930
Alexander Pototsky <silencemyalise@gmail.com>
2022-09-07 23:48:12 +0600

    v0.1-test concept

---
+++ src/BlazorAppWhoisServer/Data/TestService.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorAppWhoisServer.Data
+{
+    public class TestService
+    {   
+
+        public TestService()
+        {
+            //WhoisObserver.Services.WhoisServersClients.IpApiClient test = new();
+        }
+    }
+}
