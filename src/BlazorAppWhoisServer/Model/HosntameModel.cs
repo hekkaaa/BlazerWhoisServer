9f42e463c4647058055b658a38020b351b0707dc
Alexander Pototsky <silencemyalise@gmail.com>
2022-09-13 20:03:36 +0600

    v0.2 - test 2

--- src/BlazorAppWhoisServer/Model/HosntameModel.cs
+++ src/BlazorAppWhoisServer/Model/HosntameModel.cs
@@ -5,7 +5,7 @@ namespace BlazorAppWhoisServer.Model
     public class HosntameModel
     {
         [Required]
-        [StringLength(10, ErrorMessage = "Minimum Length 3 ", MinimumLength = 4)]
+        [StringLength(50, ErrorMessage = "Minimum Length 3 ", MinimumLength = 4)]
         public string? Hostname { get; set; }
     }
 }
